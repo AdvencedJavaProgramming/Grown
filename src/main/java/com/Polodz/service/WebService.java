4e207f9c3944882d44368d40dc6804f278513a7d
Łukasz Polatowski <ukasz Polatowski>
2016-09-07 22:48:02 +0200

    Poprawione wyswietlanie listy filmow

--- src/main/java/com/Polodz/service/WebService.java
+++ src/main/java/com/Polodz/service/WebService.java
@@ -2,6 +2,9 @@ package com.Polodz.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by lpolatowski on 2016-09-07.
  */
@@ -10,8 +13,9 @@ public class WebService {
 
 	private static String movieListString;
 
-	public String getMovieListString() {
-		return movieListString;
+	public List<String> getMovieListString() {
+		List<String> movieList = Arrays.asList(movieListString.split("\\n"));
+		return movieList;
 	}
 
 	public static void setMovieListString(String value) {
